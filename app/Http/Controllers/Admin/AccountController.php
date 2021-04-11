45ba403c7a16673e8a47bb3b6230feffb51c9c1b
Sandi Pradana <sandipradana.id@gmail.com>
2021-04-11 14:52:07 +0700

    Add update method

--- app/Http/Controllers/Admin/AccountController.php
+++ app/Http/Controllers/Admin/AccountController.php
@@ -6,12 +6,28 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AccountController extends Controller
 {
     public function profile(){
 		$user = Auth::guard('admin')->user();
 		return view('admin.account.profile', compact(['user']));
+	}
+
+	public function update(Request $request){
+		$user = Auth::guard('admin')->user();
+		$user->name		= $request->name;
+		$user->email	= $request->email;
+		$user->phone	= $request->phone;
+
+		if(strlen($request->password) > 0){
+			$user->password = Hash::make($request->password);
+		}
+
+		$user->save();
+
+		return redirect()->back();
 	}
 	
 }
